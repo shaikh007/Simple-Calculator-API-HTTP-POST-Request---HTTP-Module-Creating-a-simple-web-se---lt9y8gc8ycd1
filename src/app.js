b0288d4667d2e5d0682fcec0033ebd3d53bba403
Shaikh Irfan <shaikhji1998@gmail.com>
2023-12-29 21:53:35 +0530

    new commit

--- src/app.js
+++ src/app.js
@@ -9,20 +9,77 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Write POST endpoint to get the sum of two number
+app.post('/add', (req, res) => {
+    const { num1, num2 } = req.query;
+
+    const parsedNum1 = parseFloat(num1);
+    const parsedNum2 = parseFloat(num2);
+
+    if (isNaN(parsedNum1) || isNaN(parsedNum2)) {
+        return res.status(400).json({ status: 'error', message: 'Invalid data types' });
+    }
+
+    if (parsedNum1 > 1000000 || parsedNum2 > 1000000 || parsedNum1 + parsedNum2 > 1000000) {
+        return res.status(400).json({ status: 'error', message: 'Overflow' });
+    }
+
+    const result = parsedNum1 + parsedNum2;
+    res.json({ result });
+});
 
 
 //Write POST endpoint to get the differance of two number
+app.post('/subtract', (req, res) => {
+    const { num1, num2 } = req.body;
+
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return res.status(400).json({ status: 'error', message: 'Invalid data types' });
+    }
+
+    if (num1 < -1000000 || num2 < -1000000 || num1 - num2 < -1000000) {
+        return res.status(400).json({ status: 'error', message: 'Underflow' });
+    }
+
+    const result = num1 - num2;
+    res.json({ result });
+});
 
 
 //Write POST endpoint to get the multiplication of two number
+app.post('/multiply', (req, res) => {
+    const { num1, num2 } = req.body;
 
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return res.status(400).json({ status: 'error', message: 'Invalid data types' });
+    }
+
+    if (num1 > 1000000 || num2 > 1000000 || num1 * num2 > 1000000) {
+        return res.status(400).json({ status: 'error', message: 'Overflow' });
+    }
+
+    const result = num1 * num2;
+    res.json({ result });
+});
 
 //Write POST endpoint to check if the num2 is 0 or not and to get the result after dividing two number
+app.post('/divide', (req, res) => {
+    const { num1, num2 } = req.body;
 
-      
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return res.status(400).json({ status: 'error', message: 'Invalid data types' });
+    }
+
+    if (num2 === 0) {
+        return res.status(400).json({ status: 'error', message: 'Cannot divide by zero' });
+    }
+
+    const result = num1 / num2;
+    res.json({ result });
+});
+
 
 const server = app.listen(4000, () => {
-  console.log(`Server running on port 4000`);
+    console.log(`Server running on port 4000`);
 });
-    
+
 module.exports = app;
